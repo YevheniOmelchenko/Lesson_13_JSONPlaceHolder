9fc63452331126432f900a38203866c61644f953
Yevhen Omelchenko <evgeni.omko@gmail.com>
2023-09-08 10:51:38 +0200

    JSON finisch

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch(`${BASE_URL}/users`)
     .then((res) => res.json())
     .then((users) => {
         const userList = document.getElementById("userList");
         users.forEach((user) => {
             const li = document.createElement("li");
             li.textContent = user.name;
+            li.addEventListener("click", () => displayUserDetails(user));
             userList.appendChild(li);
         });
     })
@@ -17,3 +18,14 @@ fetch("https://jsonplaceholder.typicode.com/users")
         userList.appendChild(li);
     })
     .finally(console.log("Финиш"));
+
+function displayUserDetails(user) {
+    userDetails.innerHTML = `
+            <h2>${user.name}</h2>
+            <p><strong>Email:</strong> ${user.email}</p>
+            <p><strong>Phone:</strong> ${user.phone}</p>
+            <p><strong>Website:</strong> ${user.website}</p>
+            <p><strong>Company:</strong> ${user.company.name}</p>
+            <p><strong>Address:</strong> ${user.address.street}, ${user.address.city}</p>
+        `;
+}
